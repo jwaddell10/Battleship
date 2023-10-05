abbc119ae13cc5040e8fb0ce25e3a2d476aeedc0
jwaddell10 <jewaddell91@gmail.com>
2023-10-04 21:11:59 -0500

    update webpack

--- dist/webpack.config.js
+++ dist/webpack.config.js
@@ -1,26 +1,54 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const path = require('path');
-
 module.exports = {
-    entry: './path/to/your/entry-file.js',  // Replace with the correct path to your entry file
-    output: {
-        filename: 'bundle.js',  // The output bundled file name
-        path: path.resolve(__dirname, 'dist')  // Output directory path
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+ devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: ['node_modules'],
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.gif$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(ttf|eot|svg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      config$: './configs/app-config.js',
+      react: './vendor/react-master',
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',  // Optional: Use babel for transpiling
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
+    extensions: ['.js', '.jsx'],
+    modules: [
+      'node_modules',
+      'bower_components',
+      'shared',
+      '/shared/vendor/modules',
+    ],
+  },
 };
--- src/ship.test.js
+++ src/ship.test.js
@@ -2,20 +2,18 @@ import { Ship } from './ship.js';
 
 console.log(Ship, 'this is ship from shipjs');
 
-/*describe('the ship', () => {
+describe('the ship', () => {
     test('has length of 4', () => {
-        expect(ship1.length).toBe(3);
+        expect(Ship.length).toBe(3);
     });
     test('has 0 hits', () => {
-        expect(ship1.numberOfHits).toBeLessThan(4);
+        expect(Ship.numberOfHits).toBeLessThan(4);
     });
-})*/
+})
 
 describe('the ship', () => {
     test('has length of 4', () => {
-        const length = 4;
-        const ship = new Ship(length);
-        expect(ship.length).toBe(4);
+        expect(Ship.length).toBe(4);
     });
 
     test('has 0 hits now', () => {
