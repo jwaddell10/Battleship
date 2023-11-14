8ea2f8f2a68dd660b9e654d1a7608b3b48b8b0e6
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2023-11-14 17:48:12 -0600

    render computer attacks on playerboard

--- src/computer.js
+++ src/computer.js
@@ -1,42 +1,50 @@
 class Computer {
-    
     constructor() {
-        this.handleComputerAttack = function () {x, y}
+      this.handleComputerAttack = function () {
+        x, y;
+      };
+      this.computerShots = [];
     }
-
+  
     setAttackHandler(coords) {
-        this.handleComputerAttack = coords; // Set the handleAttack callback
+      this.handleComputerAttack = coords; // Set the handleAttack callback
     }
-
+  
     computerCheckAttack(x, y) {
-        if (x < 0 || x > 10 || y < 0 || y > 10) {
-            return;
-        }
+      if (x < 0 || x > 10 || y < 0 || y > 10) {
+        return;
+      }
     }
-    
+  
     generateRandomNumber() {
-        return Math.floor(Math.random() * 10);
-      }
-
-    checkRepeatHits(x, y, shotsArray) {
-        const coordinates = [x, y];
-        const hitsRepeat = shotsArray.some((coords) => coords[0] === x && coords[1] === y);
-
-        if (hitsRepeat === true) {
-            this.computerSendAttack();
-        } else return coordinates
-      }
-
-    computerSendAttack() {
-        const x = this.generateRandomNumber()
-        const y = this.generateRandomNumber()
-        shotsArray.push(x, y);
-        console.log(shotsArray, 'this is shotsarray')
-        this.computerCheckAttack(x, y);
-        this.checkRepeatHits(x, y, shotsArray);
-        return { x, y }
+      return Math.floor(Math.random() * 10);
     }
-}
-const shotsArray = []
-
-export default Computer
+  
+    generateAttackCoordinates() {
+      const x = this.generateRandomNumber();
+      const y = this.generateRandomNumber();
+      return { x, y };
+    }
+  
+    checkRepeatHits(x, y) {
+      const coordinates = [x, y];
+      const hitsRepeat = this.computerShots.some(
+        (coords) => coords[0] === x && coords[1] === y
+      );
+  
+      if (hitsRepeat === true) {
+        this.computerSendAttack();
+      } else return coordinates;
+    }
+  
+    computerSendAttack() {
+      const { x, y } = this.generateAttackCoordinates();
+      this.computerCheckAttack(x, y);
+      this.checkRepeatHits(x, y);
+      this.computerShots.push(x, y);
+      return this.computerShots;
+    }
+  }
+  
+  export default Computer;
+  
--- src/game.js
+++ src/game.js
@@ -1,91 +1,95 @@
-import { renderBoard, renderComputerBoard, renderPlayerShips, renderComputerShips, renderPlayerAttacks } from "./dom.js";
-import { gameBoard } from "./gameboard.js";
-import Ship, { computerShipsArray, playerShipsArray } from "./ship.js";
-import Computer from './computer.js'
-import { Player } from './player.js'
-
-
-const player = new Player('jon');
-const computer = new Computer();
-
-const playerShips = 
-    [
-    new Ship(3, 0, false, 1, 3, 'horizontal'), 
-    new Ship(4, 0, false, 5, 8, 'vertical'), 
-    new Ship(3, 0, false, 3, 4, 'horizontal')
-    ];
-    
-const computerShips = 
-    [
-    new Ship(3, 0, false, 1, 3, 'horizontal'), 
-    new Ship(4, 0, false, 2, 4, 'horizontal'), 
-    new Ship(3, 0, false, 5, 9, 'vertical')
-    ];
-
-function game() {
-
+import {
+    renderBoard,
+    renderComputerBoard,
+    renderPlayerShips,
+    renderComputerShips,
+    renderPlayerAttacks
+  } from "./dom.js";
+  import { gameBoard } from "./gameboard.js";
+  import Ship, { computerShipsArray, playerShipsArray } from "./ship.js";
+  import Computer from "./computer.js";
+  import { Player } from "./player.js";
+  
+  const player = new Player("jon");
+  const computer = new Computer();
+  
+  const playerShips = [
+    new Ship(3, 0, false, 1, 3, "horizontal"),
+    new Ship(4, 0, false, 5, 8, "vertical"),
+    new Ship(3, 0, false, 3, 4, "horizontal")
+  ];
+  
+  const computerShips = [
+    new Ship(3, 0, false, 1, 3, "horizontal"),
+    new Ship(4, 0, false, 2, 4, "horizontal"),
+    new Ship(3, 0, false, 5, 9, "vertical")
+  ];
+  
+  function game() {
     //create the players
-
+  
     //create the ships
-
+  
     //place ships on the board
     playerShipsArray.push(...playerShips);
     computerShipsArray.push(...computerShips);
-
+  
     //create the gameboards for the players and place ships on the board
     const playerGameboard = new gameBoard();
     playerGameboard.placeShips(playerShips);
     const computerGameboard = new gameBoard();
     computerGameboard.placeShips(computerShips);
-
+  
     //render the board and ships onto the DOM
     renderBoard();
     renderComputerBoard();
     renderPlayerShips();
     renderComputerShips();
-   
-    //begin the game  
-
+  
+    //begin the game
+  
     let currentPlayer = player;
     function switchPlayerTurn() {
-        if (currentPlayer === player) {
-            currentPlayer = computer;
-        } else {
-            currentPlayer = player;
-        }
+      if (currentPlayer === player) {
+        currentPlayer = computer;
+      } else {
+        currentPlayer = player;
+      }
     }
-
+  
     function playerTurn() {
-        player.sendAttack();  
-        player.setAttackHandler(function(x, y) {
-            computerGameboard.receiveAttack(computerShips, x, y);
-            switchPlayerTurn();
-            computerTurn();
-        });
+      player.sendAttack();
+      player.setAttackHandler(function (x, y) {
+        computerGameboard.receiveAttack(computerShips, x, y);
+        switchPlayerTurn();
+        computerTurn();
+      });
     }
-
+  
     function computerTurn() {
-        if (currentPlayer !== computer) {
-            return;
-        } else {
-            const attackCoordinates = computer.computerSendAttack();
-            computer.computerSendAttack();
-            playerGameboard.receiveAttack(playerShips, attackCoordinates.x, attackCoordinates.y);
-            renderPlayerAttacks();
-            switchPlayerTurn();
-        }
+      if (currentPlayer !== computer) {
+        return;
+      } else {
+        //need to make it based off shotsarray
+        const attackCoordinates = computer.computerSendAttack();
+        playerGameboard.receiveAttack(
+          playerShips,
+          attackCoordinates.x,
+          attackCoordinates.y
+        );
+        renderPlayerAttacks();
+        switchPlayerTurn();
+      }
     }
     playerTurn();
-
+  
     return {
-        playerShipsArray,
-        computerShipsArray
-    }
-}
-game();
-
-export {
-    game,
-    computer, 
-    player,
- }
+      playerShipsArray,
+      computerShipsArray
+    };
+  }
+  
+  game();
+  
+  export { game, computer, player };
+  
