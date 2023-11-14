820d7d06781a5aefaaee06e1d56a495af9f22830
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2023-11-14 17:53:42 -0600

    update commit

--- src/computer.js
+++ src/computer.js
@@ -1,50 +1,52 @@
 class Computer {
-    constructor() {
-      this.handleComputerAttack = function () {
-        x, y;
-      };
-      this.computerShots = [];
-    }
-  
-    setAttackHandler(coords) {
-      this.handleComputerAttack = coords; // Set the handleAttack callback
-    }
-  
-    computerCheckAttack(x, y) {
-      if (x < 0 || x > 10 || y < 0 || y > 10) {
-        return;
-      }
-    }
-  
-    generateRandomNumber() {
-      return Math.floor(Math.random() * 10);
-    }
-  
-    generateAttackCoordinates() {
-      const x = this.generateRandomNumber();
-      const y = this.generateRandomNumber();
-      return { x, y };
-    }
-  
-    checkRepeatHits(x, y) {
-      const coordinates = [x, y];
-      const hitsRepeat = this.computerShots.some(
-        (coords) => coords[0] === x && coords[1] === y
-      );
-  
-      if (hitsRepeat === true) {
-        this.computerSendAttack();
-      } else return coordinates;
-    }
-  
-    computerSendAttack() {
-      const { x, y } = this.generateAttackCoordinates();
-      this.computerCheckAttack(x, y);
-      this.checkRepeatHits(x, y);
-      this.computerShots.push(x, y);
-      return this.computerShots;
+
+  constructor() {
+    this.handleComputerAttack = function () {
+      x, y;
+    };
+  }
+
+  setAttackHandler(coords) {
+    this.handleComputerAttack = coords; // Set the handleAttack callback
+  }
+
+  computerCheckAttack(x, y) {
+    if (x < 0 || x > 10 || y < 0 || y > 10) {
+      return;
     }
   }
-  
-  export default Computer;
-  
+
+  generateRandomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  generateAttackCoordinates() {
+    const x = this.generateRandomNumber();
+    const y = this.generateRandomNumber();
+    return { x, y };
+  }
+
+  checkRepeatHits(x, y, shotsArray) {
+    const coordinates = [x, y];
+    const hitsRepeat = shotsArray.some(
+      (coords) => coords[0] === x && coords[1] === y
+    );
+
+    if (hitsRepeat === true) {
+      this.computerSendAttack();
+    } else return coordinates;
+  }
+
+  computerSendAttack() {
+    const { x, y } = this.generateAttackCoordinates();
+    console.log(shotsArray, "this is shotsarray");
+    this.computerCheckAttack(x, y);
+    this.checkRepeatHits(x, y, shotsArray);
+    shotsArray.push(x, y);
+    return { x, y, shotsArray };
+  }
+}
+const shotsArray = [];
+
+export default Computer;
+>>>>>>> d98e019209455566abb488ab702b2b15c579647a
